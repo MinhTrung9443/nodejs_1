2d48acb11f3682697979d998a9dbda3cba74dd55
MinhTrung9443 <minhtrungbttv@gmail.com>
2025-07-16 09:24:40 +0700

    install prettier husky lint-staged

--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -1,13 +1,13 @@
 class NewsController {
-  // [GET] /news
-  index(req, res) {
-    res.render("new");
-  }
+    // [GET] /news
+    index(req, res) {
+        res.render('new');
+    }
 
-  // [GET] /news/:slug  -- slug is a variable in the URL
-  show(req, res) {
-    res.send("News detail");
-  }
+    // [GET] /news/:slug  -- slug is a variable in the URL
+    show(req, res) {
+        res.send('News detail');
+    }
 }
 
 module.exports = new NewsController(); // khoi tao mot instance cua NewsController
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,15 @@
-const newsRouter = require("./news"); // Import the news router
-const siteRouter = require("./site"); // Import the site router
-
+const newsRouter = require('./news'); // Import the news router
+const siteRouter = require('./site'); // Import the site router
 
 function route(app) {
-  app.use("/news", newsRouter); // Use the news router for /news route
+    app.use('/news', newsRouter); // Use the news router for /news route
 
+    app.get('/search', (req, res) => {
+        console.log(req.query);
+        res.render('search');
+    });
 
-
-  app.get("/search", (req, res) => {
-    console.log(req.query);
-    res.render("search");
-  });
-
-  app.use("/", siteRouter); // Use the site router for root route
+    app.use('/', siteRouter); // Use the site router for root route
 }
 
 module.exports = route; // Export the route function
