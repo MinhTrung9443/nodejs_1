71e39a53f033023e3dd057fd8cd36a8718b6ec14
MinhTrung9443 <minhtrungbttv@gmail.com>
2025-07-16 09:38:58 +0700

    route index sử dụng use, còn file cấp nhỏ hơn phải dùng đúng phương thức get, post

--- src/routes/news.js
+++ src/routes/news.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const newsController = require('../app/controllers/NewsController');
 
-router.use('/:slug', newsController.show); // Use the show method for /news/:slug route
-router.use('/', newsController.index); // Use the index method from NewsController for /news route
+router.get('/:slug', newsController.show); // Use the show method for /news/:slug route
+router.get('/', newsController.index); // Use the index method from NewsController for /news route
 
 module.exports = router;
--- src/routes/site.js
+++ src/routes/site.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const siteController = require('../app/controllers/SiteController');
 
-router.use('/:slug', siteController.search); // Use the search method for /news/:slug route
-router.use('/', siteController.index); // Use the index method from siteController for /news route
+router.get('/:slug', siteController.search); // Use the search method for /news/:slug route
+router.get('/', siteController.index); // Use the index method from siteController for /news route
 
 module.exports = router;
