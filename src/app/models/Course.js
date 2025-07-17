d7da4b20fd27ba57f48ca5627f75bbdcca445c1a
MinhTrung9443 <minhtrungbttv@gmail.com>
2025-07-17 09:52:15 +0700

    confif at model

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,4 +1,9 @@
 const mongoose = require('mongoose');
+var slug = require('mongoose-slug-generator');
+
+var mongoose = require('mongoose');
+mongoose.plugin(slug);
+
 const Schema = mongoose.Schema;
 
 const Course = new Schema(
@@ -7,7 +12,7 @@ const Course = new Schema(
         description: { type: String, default: '', maxLength: 600 },
         image: { type: String, default: '', maxLength: 255 },
         videoId: { type: String, default: '', maxLength: 255 },
-        slug: { type: String, default: '', maxLength: 255 },
+        slug: { type: String, slug: 'name' },
         level: { type: String, default: '', maxLength: 255 },
     },
     {
